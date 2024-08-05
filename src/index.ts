402594a5dacec0c7459eb0b9cad1c9d68214e4c7
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-08-05 15:17:57 -0500

    feat:levantamos el servidor y conectamos la base de datos esta funcional

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import Express  from "express";
+import express, { Router } from 'express'
+import env from 'dotenv'
+import sequelize from "./config/db";
+import {routes} from './routes/router'
+// import corsConfig from './middleware/cors';
+
+const server = express()
+server.use(express.json())
+env.config()
+// server.use(corsConfig)
+server.use('/appi',routes)
+
+const PORT = process.env.PORT || 3001
 
 
+const startserver = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()//sincroniza la base de datos con los modelos si ya exite la tabla no la crea si no existe la crea { alter: true }
+        console.log("Database connected!")
+        server.listen(PORT, () => {
+            console.log(`server executted in http://localhost:${PORT}`)
+        })
+    } catch (error: any) {
+        console.log(`somethings wrong from index.ts`, error)
+    }
 
+}
 
-const server =Express()
-
-
-server.listen()
+startserver()
